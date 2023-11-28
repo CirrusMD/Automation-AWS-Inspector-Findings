a2280bdb98c00fef3c87c0b4d1a8330acf972750
Austin Songer <a.songer@protonmail.com>
2023-11-28 14:46:07 -0600

    Update awsinspector.py

--- awsinspector.py
+++ awsinspector.py
@@ -1,4 +1,17 @@
 import json
 import boto3
 
+def lambda_handler(event, context):
+    client = boto3.client('inspector')
 
+    # Fetch AWS Inspector findings or reports
+    findings = client.list_findings() # Additional processing or filters as needed
+
+    # Process and format the data
+    findings_data = json.dumps(findings) # Converts the response to JSON
+
+    # Return the processed data
+    return {
+        'statusCode': 200,
+        'body': findings_data
+    }
