263a378ed5e825dc908c1f2e315071c33a8f7bd9
Austin Songer <a.songer@protonmail.com>
2023-11-29 11:28:36 -0600

    Update awsinspector.py

--- awsinspector.py
+++ awsinspector.py
@@ -1,24 +1,26 @@
 import json
 import boto3
+import logging
+from botocore.exceptions import ClientError
 # TO-DO: 
 # 1. IAM Role and Permissions
 # -- inspector:ListFindings - https://docs.aws.amazon.com/inspector/v2/APIReference/API_ListFindings.html
 # -- inspector:DescribeFindings - https://docs.aws.amazon.com/inspector/v1/APIReference/API_DescribeFindings.html
-# 2. Need Logging?
-# -- import logging
-# -- from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
     client = boto3.client('inspector')
 
     # Fetch AWS Inspector findings or reports
-    findings = client.list_findings() # TO-DO: Additional processing or filters
+    findings = client.list_findings() # TO-DO: Additional processing or filters???
 
-    # Process and format the data
-    # Extracting specific details from each finding
-    findings_data = json.dumps(findings) # TO-DO: Converts the response to JSON
+    # TO-DO:
+    # - Process and format the data
+    # - Extracting specific details from each finding
+    # - Converts the response to JSON
+    findings_data = json.dumps(findings) 
 
-    # Log the processed data (or store/send it as needed)   
+    
+    # TO-DO: Log the processed data (or store/send it as needed)   
     
     
     # Return the processed data
